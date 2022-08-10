0ccf344166497baaed70dc4933c13a12a0537cf6
Patricio Cortes <patricio.cortes@noirlab.edu>
2022-08-10 10:06:53 -0400

    Create first class and two instances

---
+++ main.py
@@ -0,0 +1,22 @@
+# create first class
+class Item:
+    def __init__(self, name, price, quantity):
+        # create attributes of Item class
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    
+    # create a method
+    def calculate_total_price(self, x, y):
+        return x * y
+
+item1 = Item("Phone", 100, 5)
+item2 = Item("Laptop", 1000, 3)
+
+print(item1.name)
+print(item1.price)
+print(item1.quantity)
+
+print(item2.name)
+print(item2.price)
+print(item2.quantity)
